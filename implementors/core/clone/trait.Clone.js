4573d66f911daf9ba64af49be1507b05e6468db7
Daisuke Murase <typester@cpan.org>
2017-02-16 15:13:54 +0900

    initial commit

---
+++ implementors/core/clone/trait.Clone.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["frostflake"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='frostflake/pool/struct.GeneratorPoolOptions.html' title='frostflake::pool::GeneratorPoolOptions'>GeneratorPoolOptions</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='frostflake/pool/struct.GeneratorPool.html' title='frostflake::pool::GeneratorPool'>GeneratorPool</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='frostflake/struct.GeneratorOptions.html' title='frostflake::GeneratorOptions'>GeneratorOptions</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
